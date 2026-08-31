407f85f2d8797a978e839324d7c4752e2771e3fb
Andy123888 <ys4007@hw.ac.uk>
2026-08-31 10:41:41 +0800

    modify 7

--- robot_detection.py
+++ robot_detection.py
@@ -76,6 +76,9 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+    cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+    cap.set(cv2.CAP_PROP_EXPOSURE, 50)
+
     if not cap.isOpened():
 
         print("摄像头打开失败")
